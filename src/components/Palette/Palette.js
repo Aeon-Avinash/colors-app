3a2816937a989143bfc44bf909b51850fc869b4f
Aeon-Avinash <aeon.b.avinash@gmail.com>
2019-07-23 18:40:17 +0530

    initial CRA, added palette

---
+++ src/components/Palette/Palette.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        {/* Navbar goes here */}
+        <div className="Palette-colors">{/* bunch of color boxes */}</div>
+        {/* footer finally */}
+      </div>
+    );
+  }
+}
