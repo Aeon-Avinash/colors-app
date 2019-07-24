d8042158258956bc7334e183169a22487c5eabb7
Aeon-Avinash <aeon.b.avinash@gmail.com>
2019-07-24 11:20:02 +0530

    fix palette route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import Palette from "./components/Palette/Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
-function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/" render={() => <h1>Palette List</h1>} />
-        <Route
-          exact
-          path="/palette/:id"
-          render={() => <Palette palette={generatePalette(seedColors[4])} />}
-        />
-      </Switch>
-
-      {/* <Palette palette={generatePalette(seedColors[4])} /> */}
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.findPalette = this.findPalette.bind(this);
+  }
+  findPalette(id) {
+    return seedColors.find(palette => palette.id === id);
+  }
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path="/" render={() => <h1>Palette List</h1>} />
+          <Route
+            exact
+            path="/palette/:id"
+            render={routeProps => {
+              const { id } = routeProps.match.params;
+              const paletteFound = this.findPalette(id);
+              if (paletteFound) {
+                return <Palette palette={generatePalette(paletteFound)} />;
+              } else {
+                return <h1>Palette not found!</h1>;
+              }
+            }}
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
